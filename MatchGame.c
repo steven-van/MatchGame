6a7ce2d62ce3608df8572b15db4012a89c9ed443
steven-van <64859587+steven-van@users.noreply.github.com>
2022-09-28 00:39:51 +0200

    feat: get back to menu after game

--- MatchGame.c
+++ MatchGame.c
@@ -5,41 +5,46 @@ int main()
     int choice;
     int gameMode;
 
-    printf("Bonjour et bienvenue dans le jeu des allumettes\n\nAvant de jouer, vous pouvez consulter les regles du jeu ou les credits.\n\nQue faire ?\n\n1 - Jouer\n2 - Voir les regles du jeu\n3 - Voir les credits\n4 - Quitter\n\nVotre choix : ");
-    scanf("%d", &choice);
-    switch (choice)
+    printf("Bonjour et bienvenue dans le jeu des allumettes\n\nAvant de jouer, vous pouvez consulter les regles du jeu ou les credits.\n");
+
+    do
     {
-    case 1:
-        printf("Choisissez votre mode de jeu :\n\n1 - Humain VS Ordi - Facile\n2 - Humain VS Ordi - Difficile\n3 - 2 joueurs\n4 - Ordi VS Ordi\n\nVotre choix : ");
-        scanf("%d", &gameMode);
-        switch (gameMode)
+        printf("\nQue faire ?\n\n1 - Jouer\n2 - Voir les regles du jeu\n3 - Voir les credits\n4 - Quitter\n\nVotre choix : ");
+        scanf("%d", &choice);
+        switch (choice)
         {
         case 1:
-            onePlayerNaive();
+            printf("Choisissez votre mode de jeu :\n\n1 - Humain VS Ordi - Facile\n2 - Humain VS Ordi - Difficile\n3 - 2 joueurs\n4 - Ordi VS Ordi\n\nVotre choix : ");
+            scanf("%d", &gameMode);
+            switch (gameMode)
+            {
+            case 1:
+                onePlayerNaive();
+                break;
+            case 2:
+                onePlayerSmart();
+                break;
+            case 3:
+                twoPlayersGame();
+                break;
+            case 4:
+                computerGame();
+                break;
+            default:
+                break;
+            }
             break;
         case 2:
-            onePlayerSmart();
+            printf("Le but du jeu est simple :\n\nCe jeu se joue a deux. Vous avez un nombre n d'allumettes.\nA tour de role, chacun des deux joueurs enleve entre 1 et 3 allumettes selon ce qu'il souhaite. Le perdant est celui qui doit enlever la derniere allumette.\n\nIl existe 4 modes de jeu :\n1 - Humain VS Ordi - Facile\n2 - Humain VS Ordi - Difficile\n3 - 2 joueurs\n4 - Ordi VS Ordi\n\nBon jeu !\n");
             break;
         case 3:
-            twoPlayersGame();
+            printf("Auteur : VAN Steven\nMail : steven-van@outlook.fr\nGitHub : github.com/steven-van\n");
             break;
         case 4:
-            computerGame();
-            break;
+            printf("Bye bye ! :)");
+            exit(0);
         default:
-            break;
+            exit(0);
         }
-        break;
-    case 2:
-        printf("Le but du jeu est simple :\n\nCe jeu se joue a deux. Vous avez un nombre n d'allumettes.\nA tour de role, chacun des deux joueurs enleve entre 1 et 3 allumettes selon ce qu'il souhaite. Le perdant est celui qui doit enlever la derniere allumette.\n\nIl existe 4 modes de jeu :\n1 - Humain VS Ordi - Facile\n2 - Humain VS Ordi - Difficile\n3 - 2 joueurs\n4 - Ordi VS Ordi\n\nBon jeu !");
-        break;
-    case 3:
-        printf("Auteur : VAN Steven\nMail : steven-van@outlook.fr\nGitHub : github.com/steven-van");
-        break;
-    case 4:
-        printf("Bye bye ! :)");
-        exit(0);
-    default:
-        exit(0);
-    }
+    } while (choice < 4);
 }
